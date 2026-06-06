597a4af7e3b119bdaf714e03a8be63eacdb941c4
Hemraj Kalathoki <hemraj.kalathoki3@gmail.com>
2026-06-06 22:27:20 +0545

    update : home page UI

--- index.php
+++ index.php
@@ -719,7 +719,7 @@ include('includes/dbconnection.php');
 </button>
 
 <!-- Bootstrap -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> -->
 
 <!-- AOS -->
 <script src="https://unpkg.com/aos@2.3.4/dist/aos.js"></script>
