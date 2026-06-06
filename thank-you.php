ea3ebc2d0b1061c94b84b85d748d32549032a885
Hemraj Kalathoki <hemraj.kalathoki3@gmail.com>
2026-06-06 22:31:22 +0545

    update : thank you page UI

--- thank-you.php
+++ thank-you.php
@@ -148,7 +148,7 @@ if (strlen($_SESSION['bpmsuid']==0)) {
 <?php include_once('includes/footer.php');?>
 
 <!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> -->
 
 </body>
 </html>
